f71ad06e7ce9da7355af250751464b7f92d9c271
Sebastian-Garcia-Maciel <sebastian-maciel@outlook.com>
2021-04-22 04:43:40 -0500

    Se maqueto pantalla comienza

--- app/src/main/java/garciamaciel/sebastian/proyectofinal_thedot/meditacion_comienza.kt
+++ app/src/main/java/garciamaciel/sebastian/proyectofinal_thedot/meditacion_comienza.kt
@@ -3,10 +3,10 @@ package garciamaciel.sebastian.proyectofinal_thedot
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class meditacion_comienza : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_meditacion_comienza)
         /* Ocultar action bar */
         supportActionBar?.hide()
     }
