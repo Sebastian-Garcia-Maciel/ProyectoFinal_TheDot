6ba80ed86e259fdb5dc827b89e7333651544e06f
Sebastian-Garcia-Maciel <sebastian-maciel@outlook.com>
2021-04-27 02:39:01 -0500

    Se agrego inicio de sesión con servicio de google

--- app/src/main/java/garciamaciel/sebastian/proyectofinal_thedot/IniciaSesion.kt
+++ app/src/main/java/garciamaciel/sebastian/proyectofinal_thedot/IniciaSesion.kt
@@ -2,21 +2,39 @@ package garciamaciel.sebastian.proyectofinal_thedot
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+
 
 class IniciaSesion : AppCompatActivity() {
     @SuppressLint("WrongViewCast")
+
+    val RC_SIGN_IN = 123
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicia_sesion)
 
+
         /*Oculta la action bar*/
         supportActionBar?.hide()
-        val botonRegresar:ImageButton = findViewById<ImageButton>(R.id.boton_regresar_inicia_sesion)
+        val botonRegresar: ImageButton =
+            findViewById<ImageButton>(R.id.boton_regresar_inicia_sesion)
         val botonIniciarSesion = findViewById<Button>(R.id.boton_inicia_sesion)
+
+        val botonIniciaSesionGoogle: com.google.android.gms.common.SignInButton =
+            findViewById<com.google.android.gms.common.SignInButton>(
+                R.id.sign_in_button
+            )
 
         botonRegresar.setOnClickListener() {
             var intent: Intent = Intent(this, MainActivity::class.java)
@@ -27,5 +45,61 @@ class IniciaSesion : AppCompatActivity() {
             var intent: Intent = Intent(this, Inicio1::class.java)
             startActivity(intent)
         }
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        botonIniciaSesionGoogle.setOnClickListener {
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account)
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            // Log.w(TAG, "signInResult:failed code=" + e.statusCode)
+            updateUI(null)
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount?) {
+        if (account != null){
+            val intent:Intent = Intent(this, Inicio1::class.java)
+            intent.putExtra("name", account.displayName)
+            intent.putExtra("email", account.email)
+            startActivity(intent)
+
+        }
+    }
+
+
 }
+
--- app/src/main/java/garciamaciel/sebastian/proyectofinal_thedot/Inicio1.kt
+++ app/src/main/java/garciamaciel/sebastian/proyectofinal_thedot/Inicio1.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
 
 class Inicio1 : AppCompatActivity() {
     @SuppressLint("WrongViewCast")
@@ -16,23 +17,32 @@ class Inicio1 : AppCompatActivity() {
         /*Oculta la action bar*/
         supportActionBar?.hide()
 
-        val botonMiPerfil:ImageButton = findViewById<ImageButton>(R.id.boton_mi_perfil)
-        val botonMetodos:ImageButton = findViewById<ImageButton>(R.id.boton_metodos)
-        val  botonJournal:ImageButton = findViewById<ImageButton>(R.id.boton_journal)
-        val  botonAbout:ImageButton = findViewById<ImageButton>(R.id.boton_about)
+        val botonMiPerfil: ImageButton = findViewById<ImageButton>(R.id.boton_mi_perfil)
+        val botonMetodos: ImageButton = findViewById<ImageButton>(R.id.boton_metodos)
+        val botonJournal: ImageButton = findViewById<ImageButton>(R.id.boton_journal)
+        val botonAbout: ImageButton = findViewById<ImageButton>(R.id.boton_about)
+        val headerConNombre:TextView = findViewById(R.id.tv_header)
 
+        val bundle = intent.extras
+        if (bundle != null){
+            val nombre = bundle.getString("name")
+            val correo = bundle.getString("email")
 
+            headerConNombre.setText(nombre)
 
-        botonMiPerfil.setOnClickListener() {
-            var intent: Intent = Intent(this, MiPerfil::class.java)
-            startActivity(intent)
         }
+
+
+            botonMiPerfil.setOnClickListener() {
+                var intent: Intent = Intent(this, MiPerfil::class.java)
+                startActivity(intent)
+            }
         botonMetodos.setOnClickListener() {
             var intent: Intent = Intent(this, Metodos::class.java)
             startActivity(intent)
         }
 
-         botonJournal.setOnClickListener() {
+        botonJournal.setOnClickListener() {
             var intent: Intent = Intent(this, Journal::class.java)
             startActivity(intent)
         }
