8d5f115b6bb7cc9850bc6a870f5e5abd65277f45
Sebastián García Maciel <sebastian-maciel@outlook.com>
2021-05-23 18:49:58 -0500

    Se detallo apartado mi perfil

--- app/src/main/java/garciamaciel/sebastian/proyectofinal_thedot/ui/ui/miPerfil/MiPerfil.kt
+++ app/src/main/java/garciamaciel/sebastian/proyectofinal_thedot/ui/ui/miPerfil/MiPerfil.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.LinearLayout
 import garciamaciel.sebastian.proyectofinal_thedot.R
 import garciamaciel.sebastian.proyectofinal_thedot.ui.ui.menu.ActivityInicio
 import garciamaciel.sebastian.proyectofinal_thedot.ui.ui.metodos.Metodos
@@ -18,10 +19,10 @@ class MiPerfil : AppCompatActivity() {
         setContentView(R.layout.activity_mi_perfil)
         supportActionBar?.hide()
         val btnRegresar: ImageButton = findViewById(R.id.btnRegresarPerfil) as ImageButton
-        val btnRecordatorios: Button = findViewById(R.id.btnRecordatorios) as Button
-        val btnAfirmaciones: Button = findViewById(R.id.btnAfirmaciones) as Button
-        val btnMetas: Button = findViewById(R.id.btnMetas) as Button
-        val btnTecnicas: Button = findViewById(R.id.btnAgenda) as Button
+        val btnRecordatorios: LinearLayout = findViewById(R.id.btnRecordatorios) as LinearLayout
+        val btnAfirmaciones: LinearLayout = findViewById(R.id.btnAfirmaciones) as LinearLayout
+        val btnMetas: LinearLayout = findViewById(R.id.btnMetas) as LinearLayout
+        val btnTecnicas: LinearLayout = findViewById(R.id.btnAgenda) as LinearLayout
 
 
         btnRegresar.setOnClickListener{
